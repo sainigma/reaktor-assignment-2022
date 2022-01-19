469dd778d61cadcfc94c71dbe7243b49cf2ba7bb
sainigma <sainigma@gmail.com>
2022-01-19 15:05:50 +0200

    configured webpack for building & development server

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const init = () => {
+  const target = document.getElementById("asd")
+  target.innerHTML = "Webpack toimii"
+}
+
+window.onload = init
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const buildMode = process.env.npm_lifecycle_event;
+
+let config = {
+  entry: {
+    main: ['./src/app.js']
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    })
+  ]
+}
+
+if (buildMode === 'dev' || !buildMode) {
+  console.log("Development mode")
+  config.mode = 'development'
+  config.devServer = {
+    static: {
+      directory: path.resolve(__dirname, 'build')
+    },
+    compress: false,
+    port: 8000
+  }
+} else {
+  console.log("Production mode")
+  config.mode = 'production'
+}
+
+module.exports = (env, argv) => {
+  return config
+}
